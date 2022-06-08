b853a97c0732a3e536e84156197ec39134638c32
mohmaedelaraby <elarabym857@gmail.com>
2022-06-08 13:13:01 +0200

    "some minpulation"

--- src/View/Home/HomePage.vue
+++ src/View/Home/HomePage.vue
@@ -2,6 +2,10 @@
 <template>
   <div>
     <h1>{{this.$attrs.props.title}}</h1>
+    <p>{{this.$attrs.props.count}}</p>
+    <p>{{t}}</p>
+    <button @click="this.$attrs.props.fu(t)">Count - {{t}}</button>
+     <button @click="add">Count - {{t}}</button>
   </div>
 </template>
 
@@ -13,21 +17,38 @@ export default {
   data(){
     return{
       txt:"DSdsds",
-      t: this.$attrss
+      t: this.$attrs.props.count
     }
   },
   props: {
     title: {
       type: String,
       required: true
+    },
+     count: {
+      type: Number
     }
-  },
+    ,
+    fu:{
+      type: Function
+      }
+
+  },methods:{
+    add(){
+      this.t++;
+      console.log("in fun")
+    }
+  }
  
 };
 </script>
 
 <style lang="css" scoped>
 h1 {
+  color: red;
+}
+p{
+  font-size: 25px;
   color: red;
 }
 </style>
